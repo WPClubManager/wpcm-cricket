51cef36a17da46beb058759cbe8e883722493b43
Iain <iain@polevaultweb.com>
2026-04-05 17:52:49 +0100

    Fix: install wp-club-manager dependency in test workflow, add WPCM stubs for PHPStan

---
+++ phpstan-stubs/wpcm.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! class_exists( 'WPCM_Widget' ) ) {
+    class WPCM_Widget extends WP_Widget {}
+}
+if ( ! function_exists( 'WPCM' ) ) {
+    function WPCM() { return new stdClass(); }
+}
